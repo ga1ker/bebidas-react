e63d3ced934bdfb64ec76fcd05d8551da391acc8
Gadiel Alcazar Bernal <galkker@MacBookAir.mshome.net>
2025-02-12 15:04:13 -0600

    06 show DrinkCard components in HomePage

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
-import { CategoriesAPIResponseSchema, RecipesAPIResponseSchema, SearchFiltersSchema } from "../utils/recipes-schema";
+import { CategoriesAPIResponseSchema, DrinkSchema, RecipesAPIResponseSchema, SearchFiltersSchema } from "../utils/recipes-schema";
 
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>
 export type SearchFilters = z.infer<typeof SearchFiltersSchema>
 export type Recipes = z.infer<typeof RecipesAPIResponseSchema>
+export type Drink = z.infer<typeof DrinkSchema>
