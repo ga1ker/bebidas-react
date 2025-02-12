f7879abc03f85d17ee75c50dcddac819fd972c25
Gadiel Alcazar Bernal <galkker@MacBookAir.mshome.net>
2025-02-12 15:58:04 -0600

    07 Implements Modal Component

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { CategoriesAPIResponseSchema, RecipesAPIResponseSchema } from "../utils/recipes-schema";
-import { SearchFilters } from "../types";
+import { CategoriesAPIResponseSchema, RecipeAPIResponseSchema, RecipesAPIResponseSchema } from "../utils/recipes-schema";
+import { Drink, SearchFilters } from "../types";
 
 export async function getCategories() {
     const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
@@ -15,6 +15,15 @@ export async function getRecipes(filters: SearchFilters) {
     const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${filters.category}`
     const { data } = await axios(url);
     const result = RecipesAPIResponseSchema.safeParse(data)
+    if (result.success) {
+        return result.data
+    }
+}
+
+export async function getRecipeById(id: Drink['idDrink']) {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+    const { data } = await axios(url);
+    const result = RecipeAPIResponseSchema.safeParse(data.drinks[0])
     if (result.success) {
         return result.data
     }
