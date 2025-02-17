6b05606ef2863f0a2c22d6db3bde3c41e6510cd3
ga1ker <zenget008@gmail.com>
2025-02-17 13:26:40 -0600

    10 Implements localstorage for favorites

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -5,6 +5,7 @@ export type FavoriteSliceType = {
     favorites: Recipe[]
     addFavorites: (recipe: Recipe) => void
     favoriteExist: (id: Recipe['idDrink']) => boolean;
+    loadFavorites: () => void
 }
 
 export const createFavoriteSlice: StateCreator<FavoriteSliceType> = (set, get) => ({
@@ -22,8 +23,15 @@ export const createFavoriteSlice: StateCreator<FavoriteSliceType> = (set, get) =
                 favorites: [...get().favorites, recipe]
             })
         }
+        localStorage.setItem('favorites', JSON.stringify(get().favorites))
     },
     favoriteExist: (id) => {
         return get().favorites.some(d => d.idDrink === id)
+    },
+    loadFavorites: () => {
+        const dataStorage = localStorage.getItem('favorites')
+        set({
+            favorites: dataStorage? JSON.parse(dataStorage) : []
+        })
     }
 })
